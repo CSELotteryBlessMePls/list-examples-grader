ebe2900605c3b67f674a63240d98f5c06ad00b89
xjramirez <xavierramirez112902@gmail.com>
2022-11-28 02:22:46 -0800

    update all to what it was before i think???

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,10 +1,47 @@
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
 import org.junit.*;
 
 public class TestListExamples {
-  @Test(timeout = 100)
-  public void testTimeout() {
-    while(true) {}
-  }
   // Write your grading tests here!
+  @Test
+  public void testMerge() {
+
+    ArrayList<String> list1 = new ArrayList<>();
+    ArrayList<String> list2 = new ArrayList<>();
+    list1.add("aa");
+    list1.add("bb");
+    list1.add("ca");
+    list1.add("da");
+    list1.add("db");
+
+    list2.add("ab");
+    list2.add("ba");
+    list2.add("cb");
+    
+    ArrayList<String> result = new ArrayList<>();
+    result.add("aa"); result.add("ab"); result.add("ba"); result.add("bb"); 
+    result.add("ca"); result.add("cb"); result.add("da"); result.add("db");
+    assertArrayEquals(result.toArray(), ListExamples.merge(list1, list2).toArray()); 
+  }
+
+  class CheckIsEvenLetters implements StringChecker {
+
+    public boolean checkString(String s) {
+      return s.length() % 2 == 0;
+    }
+  }
+
+  @Test
+  public void testFilter(){
+    StringChecker check = new CheckIsEvenLetters();
+    ArrayList<String> list1 = new ArrayList<>();
+    ArrayList<String> expectedArray = new ArrayList<>();
+    list1.add("apple");
+    list1.add("fortnite"); expectedArray.add("fortnite");
+    list1.add("123456"); expectedArray.add("123456");
+    assertArrayEquals(expectedArray.toArray(), ListExamples.filter(list1, check).toArray());
+  }
 }
--- grade.sh
+++ grade.sh
@@ -1,9 +1,46 @@
 # Create your grading script here
 
-rm -rf student-submission
+#set -e
+
+#clear the folder for each now run of the bash file
+rm -rf student-submission 
+#clone the repository to the root of the directory
 git clone $1 student-submission
-echo 'Finished cloning'
+#copy the file TestListExamples.java into the main folder (that we just cloned everything into)
+cp TestListExamples.java student-submission/
+cp -r lib student-submission/
+#change the directory into the foler
+cd student-submission
 
-
-
-
+#if ListExamples.java exists within the folder
+if [[ -f ListExamples.java ]]
+then #file found, move on
+    echo "file found"
+else #file not found, tell that, print the grade which is still zero, exit with the error exit code
+    echo "file not found"
+    echo "failed"
+    exit 1
+fi
+#comple the java files, report the output to compile-out.txt
+javac -cp ".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar" *.java 2> compile-output.txt
+#if no errors
+if [[ $? -eq 0 ]]
+then
+    echo "compiled fine"
+    java -cp ".:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples > errors.txt
+    ERROR=$(grep "FAILURES" errors.txt)
+    if [[ $ERROR == "FAILURES!!!" ]]
+    then
+        echo "failed"
+        echo "errors:"
+        cat errors.txt
+        exit 
+    fi
+    echo "Tests Passed! Congrats!"
+    exit
+else
+    cat compile-output.txt
+    echo "compile error"
+    echo "failed"
+    exit 1
+fi
